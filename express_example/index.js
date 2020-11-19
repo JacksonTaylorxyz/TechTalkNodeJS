d1af10f9b3a5a2e28829d82a043b244f26fd91f1
JTaylor <jtaylor@classicalconversations.com>
2020-11-19 16:52:09 -0500

    Add comments to explain express app

--- express_example/index.js
+++ express_example/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+// Create an instance of an express app
 const app = express();
 
+// Handle requests made to localhost:8080/, and run this function
 app.get('/', function(req, res) {
     console.log('Request at / ...');
     res.send('Hello, World!');
 });
 
+// Begin listening.
 app.listen(8080, function() {
     console.log('Listening...');
 });
