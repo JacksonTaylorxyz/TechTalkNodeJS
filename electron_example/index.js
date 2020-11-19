237aacd78301f8d4d606474a1b3eb82c82702962
JTaylor <jtaylor@classicalconversations.com>
2020-11-19 16:51:58 -0500

    Add comments to explain electron app

--- electron_example/index.js
+++ electron_example/index.js
@@ -2,10 +2,14 @@ const {app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
+// Global window instance
 let win;
 
+// Used as the callback for when the app is ready.
 function createWindow() {
     win = new BrowserWindow({width: 640, height: 480});
+
+    // Load an html page into the window
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
@@ -14,6 +18,7 @@ function createWindow() {
     console.log('Window started');
 }
 
+// When the environment is ready, call to create a window
 app.on('ready', createWindow);
 console.log('End of file');
 
